e762be17c19e3ab9b5f11790859e8aa47cec2b25
Alex@gdplace.com <Alex@gdplace.com>
2023-11-22 22:03:09 -0500

    added doubleclick prevention and clear grid

--- src/App.js
+++ src/App.js
@@ -34,6 +34,8 @@ const Grid = () => {
     const card = ranks[rankIndex] + ' of ' + suits[suitIndex];
     return ranks[rankIndex] !== undefined ? card : ''; // Return an empty string if card is undefined
   }
+
+  
   
 
   const handleClick = (row, col) => {
@@ -43,7 +45,13 @@ const Grid = () => {
       alert('Please enter a valid number.');
       return;
     }
-
+  
+    // Check if the cell already has a value (different from -1)
+    if (grid[row][col] !== -1) {
+      alert('This cell already contains a value.');
+      return;
+    }
+  
     // Update the grid with the input number
     const newGrid = grid.map((rowArray, rowIndex) =>
       rowArray.map((cell, colIndex) =>
@@ -52,17 +60,24 @@ const Grid = () => {
     );
     setGrid(newGrid);
     setInputNumber(''); // Clear the input after setting the number
-
+  
     // Update row and column sums
     const newRowSums = newGrid.map((rowArray) =>
       rowArray.reduce((sum, cell) => sum + (cell || 0), 0)
     );
     setRowSums(newRowSums);
-
+  
     const newColSums = newGrid[0].map((_, colIndex) =>
       newGrid.reduce((sum, rowArray) => sum + (rowArray[colIndex] || 0), 0)
     );
     setColSums(newColSums);
+  };
+  const clearGrid = () => {
+    setGrid(Array(5).fill(Array(5).fill(-1))); // Reset the grid to its initial state
+    setInputNumber(''); // Clear the input field
+    setRowSums(Array(5).fill(0)); // Reset row sums
+    setColSums(Array(5).fill(0)); // Reset column sums
+    setGeneratedNumbers([]); // Clear generated numbers array
   };
 
   return (
@@ -74,6 +89,9 @@ const Grid = () => {
       />
       <button onClick={generateRandomNumber}>
         Generate Unique Random Number
+      </button>
+      <button onClick={clearGrid}>
+        Clear Grid
       </button>
 
       {grid.map((rowArray, rowIndex) => (
@@ -92,6 +110,7 @@ const Grid = () => {
     </div>
   );
 };
+
 
   
 
